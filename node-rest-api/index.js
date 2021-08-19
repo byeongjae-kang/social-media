e31bfddbb446274772040269aaf1eb2436902bce
Byeongjae Kang <bart.kang.0719@gmail.com>
2021-08-18 21:22:13 -0700

    import routes and add them to middleware

--- node-rest-api/index.js
+++ node-rest-api/index.js
@@ -4,27 +4,34 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const helmet = require("helmet");
 const morgan = require("morgan");
+const userRoute = require("./routes/users");
+const authRoute = require("./routes/auth");
 
 dotenv.config();
 
-mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true }, () => {
-  console.log('connected to MongoDB')
-})
+mongoose.connect(
+  process.env.MONGO_URL,
+  { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true },
+  () => {
+    console.log("connected to MongoDB");
+  }
+);
 
 //middleware
 
 app.use(express.json());
-app.use(helmet())
-app.use(morgan('dev'))
+app.use(helmet());
+app.use(morgan("dev"));
 
-app.get('/', (req, res) => {
-  res.send('welcome to homepage');
-})
-app.get('/users', (req, res) => {
-  res.send('welcome to user page');
-})
+app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
 
 
+
+
+app.get("/", (req, res) => {
+  res.send("welcome to homepage");
+});
 
 app.listen(8800, () => {
   console.log(`listening to port 8800`);
