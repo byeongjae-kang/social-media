b21eefcaf57e353860d769c60f16f13969944df2
Byeongjae Kang <bart.kang.0719@gmail.com>
2021-08-19 22:27:07 -0700

    import and render home component

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,6 @@
-function App() {
-  return (
-    <div>
-      hello
-    </div>
-  );
+import Home from "./pages/home/Home";
+
+export default function App() {
+  return <Home />;
 }
 
-export default App;
