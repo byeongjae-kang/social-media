5113e2a786d308201c22c44b0fbf9513f215fbc8
Byeongjae Kang <bart.kang.0719@gmail.com>
2021-08-21 22:54:50 -0700

    render home page now

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,10 @@
-// import Home from "./pages/home/Home";
-import Profile from "./pages/profile/Profile";
+import Home from "./pages/home/Home";
+// import Profile from "./pages/profile/Profile";
+// import Login from "./pages/login/Login";
+// import Register from "./pages/register/Register";
 
 export default function App() {
   return (
-  <Profile />);
+  <Home />);
 }
 
