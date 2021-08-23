04b6fdedfbed632def9e1b8baaeb3a53524ee1b7
Byeongjae Kang <bart.kang.0719@gmail.com>
2021-08-22 21:59:18 -0700

    implement login functionality

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -1,6 +1,21 @@
+import { useContext, useRef } from "react";
+import { loginCall } from "../../apiCalls";
+import { AuthContext } from "../../context/AuthContext";
 import "./login.css";
+import { CircularProgress } from "@material-ui/core";
 
 export default function Login() {
+  const email = useRef();
+  const password = useRef();
+  const { user, isFetching, error, dispatch } = useContext(AuthContext);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    loginCall(
+      { email: email.current.value, password: password.current.value },
+      dispatch
+    );
+  };
   return (
     <div className="login">
       <div className="loginWrapper">
@@ -10,16 +25,39 @@ export default function Login() {
             Connect with friends and the world around you on SocialMedia
           </span>
         </div>
-        <div className="loginRight">
-          <div className="loginBox">
-            <input placeholder="Email" className="loginInput" />
-            <input placeholder="Password" className="loginInput" />
-            <button className="loginButton">Log In</button>
-            <span className="loginForgot">Forgot Password?</span>
-            <button className="loginRegisterButton">
-              Create a New Account
+        <div className="loginRight" onSubmit={submitHandler}>
+          <form className="loginBox">
+            <input
+              placeholder="Email"
+              type="email"
+              required
+              className="loginInput"
+              ref={email}
+            />
+            <input
+              placeholder="Password"
+              type="password"
+              className="loginInput"
+              minLength="6"
+              required
+              ref={password}
+            />
+            <button className="loginButton" disabled={isFetching}>
+              {isFetching ? (
+                <CircularProgress color="white" size="20px" />
+              ) : (
+                "Log In"
+              )}
             </button>
-          </div>
+            <span className="loginForgot">Forgot Password?</span>
+            <button className="loginRegisterButton" disabled={isFetching}>
+              {isFetching ? (
+                <CircularProgress color="white" size="20px" />
+              ) : (
+                "Create a New Account"
+              )}
+            </button>
+          </form>
         </div>
       </div>
     </div>
