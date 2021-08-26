efb1609f7ae3becdc19153599caf88753da7b6f2
Byeongjae Kang <bart.kang.0719@gmail.com>
2021-08-26 14:10:05 -0700

    import dependencies and implement to save uploaded files to disk storage

--- node-rest-api/index.js
+++ node-rest-api/index.js
@@ -4,6 +4,8 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const helmet = require("helmet");
 const morgan = require("morgan");
+const multer = require("multer");
+const path = require("path");
 const authRoute = require("./routes/auth");
 const usersRoute = require("./routes/users");
 const postsRoute = require("./routes/posts");
@@ -23,13 +25,31 @@ mongoose.connect(
 app.use(express.json());
 app.use(helmet());
 app.use(morgan("dev"));
+app.use("/images", express.static(path.join(__dirname, "public/images")));
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "public/images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, req.body.name);
+  }
+});
+
+const upload = multer({ storage: storage });
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  console.log(upload)
+  console.log(storage)
+  try {
+    return res.status(200).json("File uploded successfully");
+  } catch (error) {
+    console.error(error);
+  }
+});
 
 app.use("/api/auth", authRoute);
 app.use("/api/users", usersRoute);
 app.use("/api/posts", postsRoute);
-
-
-
 
 app.get("/", (req, res) => {
   res.send("welcome to homepage");
